bd4fee78ba8f7bde2d7071ef88441798a79a260b
Hery <hery@ittron.co.id>
2021-10-12 01:57:55 +0700

    update

---
+++ src/extension.ts
@@ -0,0 +1,14 @@
+import { languages, ExtensionContext } from 'vscode';
+import LinkProvider from './providers/linkProvider';
+import HoverProvider from './providers/hoverProvider';
+
+export function activate(context: ExtensionContext) {
+    let hover = languages.registerHoverProvider(['php', 'blade'], new HoverProvider());
+    let link = languages.registerDocumentLinkProvider(['php', 'blade'], new LinkProvider());
+
+    context.subscriptions.push(hover, link);
+}
+
+export function deactivate() {
+    //
+}
