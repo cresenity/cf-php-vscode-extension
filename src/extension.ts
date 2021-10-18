061e6b541daae0e8d2c3d8320b35435f80433800
Hery <hery@ittron.co.id>
2021-10-19 06:34:20 +0700

    update 1.0.7 - just activate this extension on cf project

--- src/extension.ts
+++ src/extension.ts
@@ -1,18 +1,34 @@
 import * as vscode from 'vscode';
 import LinkProvider from './providers/linkProvider';
 import HoverProvider from './providers/hoverProvider';
-
+import * as path from "path";
+import * as fs from "fs";
 export function activate(context: vscode.ExtensionContext) {
-    vscode.window.showInformationMessage('PHP CF Activated');
-    console.log('PHP CF Activated');
-    let hover = vscode.languages.registerHoverProvider(['php', 'blade'], new HoverProvider());
-    let link = vscode.languages.registerDocumentLinkProvider(['php', 'blade'], new LinkProvider());
 
-    let disposable = vscode.commands.registerCommand('phpcf.showTodo', () =>{
-        vscode.window.showInformationMessage('Todo from phpcf');
-    });
+    //check is cf project
 
-    context.subscriptions.push(hover, link);
+    let isCF = false;
+    if(vscode.workspace.workspaceFolders.length>0) {
+        vscode.workspace.workspaceFolders.forEach(element => {
+            let root = element.uri.fsPath;
+            let cfFile = root + path.sep + 'cf';
+            if (fs.existsSync(cfFile)) {
+                isCF=true;
+            }
+        });
+    }
+
+    if(isCF) {
+        vscode.window.showInformationMessage('PHP CF Activated');
+        let hover = vscode.languages.registerHoverProvider(['php', 'blade'], new HoverProvider());
+        let link = vscode.languages.registerDocumentLinkProvider(['php', 'blade'], new LinkProvider());
+
+        let disposable = vscode.commands.registerCommand('phpcf.showTodo', () =>{
+            vscode.window.showInformationMessage('Todo from phpcf');
+        });
+
+        context.subscriptions.push(hover, link);
+    }
 }
 
 export function deactivate() {
