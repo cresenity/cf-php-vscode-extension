f468481f410219b7596d7a2b79197466b5ccc53d
Hery <hery@ittron.co.id>
2021-10-19 09:15:07 +0700

    update

--- src/extension.ts
+++ src/extension.ts
@@ -3,6 +3,8 @@ import LinkProvider from './providers/linkProvider';
 import HoverProvider from './providers/hoverProvider';
 import * as path from "path";
 import * as fs from "fs";
+import { AppModel } from './live/appModel';
+import { checkNewAnnouncement } from './announcement';
 export function activate(context: vscode.ExtensionContext) {
 
     //check is cf project
@@ -19,6 +21,8 @@ export function activate(context: vscode.ExtensionContext) {
     }
 
     if(isCF) {
+        const appModel = new AppModel();
+        checkNewAnnouncement(context.globalState);
         vscode.window.showInformationMessage('PHP CF Activated');
         let hover = vscode.languages.registerHoverProvider(['php', 'blade'], new HoverProvider());
         let link = vscode.languages.registerDocumentLinkProvider(['php', 'blade'], new LinkProvider());
@@ -27,7 +31,36 @@ export function activate(context: vscode.ExtensionContext) {
             vscode.window.showInformationMessage('Todo from phpcf');
         });
 
-        context.subscriptions.push(hover, link);
+
+        context.subscriptions.push(vscode.commands
+            .registerCommand('extension.phpcf.goOnline', async (fileUri) => {
+                await vscode.workspace.saveAll();
+                appModel.Golive(fileUri ? fileUri.fsPath : null);
+            })
+        );
+
+        context.subscriptions.push(vscode.commands
+            .registerCommand('extension.phpcf.goOffline', () => {
+                appModel.GoOffline();
+            })
+        );
+
+        context.subscriptions.push(vscode.commands
+            .registerCommand('extension.phpcf.changeWorkspace', () => {
+                appModel.changeWorkspaceRoot();
+            })
+        );
+
+        // context.subscriptions.push(window
+        //     .onDidChangeActiveTextEditor(() => {
+        //         if (window.activeTextEditor === undefined) return;
+        //         if (workspace.rootPath === undefined && Helper.IsSupportedFile(window.activeTextEditor.document.fileName)) {
+        //             StatusbarUi.Init();
+        //         }
+        //     })
+        // );
+
+        context.subscriptions.push(hover, link, appModel);
     }
 }
 
