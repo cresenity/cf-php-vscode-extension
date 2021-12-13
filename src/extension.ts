2cac1c088699ab73c255975b7786ca8856c6a589
Haris Thohir <me@harisuddin.com>
2021-12-13 20:35:41 +0700

    validate js path

--- src/extension.ts
+++ src/extension.ts
@@ -14,17 +14,17 @@ export function activate(context: vscode.ExtensionContext) {
     //check is cf project
 
     let isCF = false;
-    if(vscode.workspace.workspaceFolders.length>0) {
+    if (vscode.workspace.workspaceFolders.length > 0) {
         vscode.workspace.workspaceFolders.forEach(element => {
             let root = element.uri.fsPath;
             let cfFile = root + path.sep + 'cf';
             if (fs.existsSync(cfFile)) {
-                isCF=true;
+                isCF = true;
             }
         });
     }
 
-    if(isCF) {
+    if (isCF) {
 
         //register command
         try {
@@ -33,11 +33,27 @@ export function activate(context: vscode.ExtensionContext) {
             reportError(error, 'initCommands');
         }
 
+        vscode.workspace.onDidSaveTextDocument((document: vscode.TextDocument) => {
+            // logger.info(JSON.stringify(document.uri.fsPath));
+            let workspaceFolder = vscode.workspace.getWorkspaceFolder(document.uri).uri.fsPath;
+            let relativePath = path.relative(workspaceFolder, document.uri.fsPath);
+            let relativePathExploded = relativePath.split(path.sep);
+
+            logger.info(relativePath);
+            logger.info(JSON.stringify(relativePathExploded));
+
+            if(relativePathExploded.length >= 4){
+                let isValid = relativePathExploded[0] == 'application' && relativePathExploded[3] == 'js';
+                logger.info(`isValid ${isValid}`);
+                logger.info('TODO:build');
+            }
+
+        });
         checkNewAnnouncement(context.globalState);
 
         app.statusBar.show();
 
-        showInformationMessage('PHP CF Activated');
+        showInformationMessage('PHP CF is Activated');
         logger.info('PHP CF Activated');
         let hover = vscode.languages.registerHoverProvider(['php', 'blade'], new HoverProvider());
         let link = vscode.languages.registerDocumentLinkProvider(['php', 'blade'], new LinkProvider());
