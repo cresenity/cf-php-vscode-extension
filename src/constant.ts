8cef187d9a092d82ba5d33b5ce6fab4da99bb4b0
Hery <hery@ittron.co.id>
2024-06-10 17:14:57 +0700

    update view regex

--- src/constant.ts
+++ src/constant.ts
@@ -1,4 +1,4 @@
-export const VIEW_REGEX = "(?<=view\\(|@include\\(|@extends\\(|@component\\(|addView\\(|setView\\(|CView::factory\\(|Inertia::render\\()(['\"])[^'\"]*\\1";
+export const VIEW_REGEX = "(?<=view\\(|@include\\(|@extends\\(|@component\\(|addView\\(|setView\\(|setViewLoginName\\(|CView::factory\\(|Inertia::render\\()(['\"])[^'\"]*\\1";
 export const CONTROLLER_URL_REGEX = "(?<=curl::redirect\\(|c::redirect\\(|c::url\\()(['\"])[^'\"]*\\1";
 export const EXTENSION_NAME = 'phpcf';
 
